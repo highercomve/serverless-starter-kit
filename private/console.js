33c385523d9e5d335d70dd126ecdd859c5550698
Sergio Marin <higher.vnf@gmail.com>
2017-08-06 12:03:59 -0400

    Change console initialization

--- private/console.js
+++ private/console.js
@@ -4,20 +4,6 @@ var fs = require('fs')
 var path = require('path')
 var glob = require('glob')
 
-// Load ENV vars from ecosystem-dev.json
-try {
-  console.log('Loading enviroment variables from "../environments/development.json"')
-  var filePath = path.join(__dirname, '../environments/development.json')
-  var fileContent = fs.readFileSync(filePath, 'utf8')
-  var envVars = JSON.parse(fileContent)
-  Object.keys(envVars).forEach(function (key) {
-    process.env[key] = envVars[key]
-  })
-} catch (e) {
-  console.log('Unable to find "../environments/development.json", loading directly from environment')
-}
-// All variables are loaded
-
 // Load all modules in src
 function loadVariables () {
   var AllJsFilesPattern = path.join(__dirname, '../src/**/*.js')
@@ -30,6 +16,8 @@ function loadVariables () {
 }
 
 var initialContext = loadVariables()
+// Load handler and initializate mongo
+initialContext['handlers'] = require('../handler.js')
 
 var replServer = repl.start({
   prompt: 'Api > '
