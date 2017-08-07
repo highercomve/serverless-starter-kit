bd1bc92421788b9b29564d82b4bed067247c73b8
Sergio Marin <higher.vnf@gmail.com>
2017-08-06 22:32:52 -0400

    Use mongoclient on connection

--- handler.js
+++ handler.js
@@ -1,6 +1,6 @@
 require('./load_env_vars')
 const Mongoose = require('mongoose')
-const connectOptions = { user: process.env.MONGO_USER, pass: process.env.MONGO_PASS, useMongoClient: true }
+const connectOptions = { useMongoClient: true }
 Mongoose.connect(process.env.MONGO_URL, connectOptions)
 
 // Load handlers
